a7b0e61d0a0fd1b0bd7e66ce503b3b259c7e87b8
lee-ranaweer <lee.ranaweer@gmail.com>
2025-05-15 12:20:46 -0400

    External link icons

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -1,13 +1,45 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+
 const Links = () => {
     return (
         <section className="links">
             <h1>Links</h1>
-            <ul>
-                <li><a href="https://github.com/lee-ranaweer">GitHub</a></li>
-                <li><a href="https://www.linkedin.com/in/nelith-ranaweera/">LinkedIn</a></li>
+            <ul className="icon-list">
+                <li>
+                    <a
+                        href="https://github.com/lee-ranaweer"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="GitHub"
+                    >
+                        <FaGithub className="icon" />
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        href="https://www.linkedin.com/in/nelith-ranaweera/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="LinkedIn"
+                    >
+                        <FaLinkedin className="icon" />
+                    </a>
+                </li>
+                
+                <li>
+                    <a
+                        href="mailto:nranawee@uoguelph.ca"
+                        aria-label="Email"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        <FaEnvelope className="icon" />
+                    </a>
+                </li>
             </ul>
         </section>
     );
 }
 
-export default Links;
+export default Links;   
